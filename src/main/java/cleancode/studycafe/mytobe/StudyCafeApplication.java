b64179a269b9181eb0b77f269e700fbfc3030710
taehwan0 <jang_taehwan@naver.com>
2026-02-02 18:53:39 +0900

    chore: my tobe 추가

---
+++ src/main/java/cleancode/studycafe/mytobe/StudyCafeApplication.java
@@ -0,0 +1,10 @@
+package cleancode.studycafe.mytobe;
+
+public class StudyCafeApplication {
+
+	public static void main(String[] args) {
+		StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+		studyCafePassMachine.run();
+	}
+
+}
---
+++ src/main/java/cleancode/studycafe/mytobe/exception/AppException.java
@@ -0,0 +1,9 @@
+package cleancode.studycafe.mytobe.exception;
+
+public class AppException extends RuntimeException {
+
+	public AppException(String message) {
+		super(message);
+	}
+
+}

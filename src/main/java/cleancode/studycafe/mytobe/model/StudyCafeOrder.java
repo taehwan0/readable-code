90e4583c3d489b8263088c5154f31c72e43672aa
taehwan0 <jang_taehwan@naver.com>
2026-02-04 12:41:10 +0900

    chore: tobe package 누락 클래스 추가

---
+++ src/main/java/cleancode/studycafe/mytobe/model/StudyCafeOrder.java
@@ -0,0 +1,31 @@
+package cleancode.studycafe.mytobe.model;
+
+import cleancode.studycafe.mytobe.exception.AppException;
+import java.util.Optional;
+
+public class StudyCafeOrder {
+
+    private final StudyCafePass studyCafePass;
+    private final StudyCafeLockerPass studyCafeLockerPass;
+
+    private StudyCafeOrder(StudyCafePass studyCafePass, StudyCafeLockerPass studyCafeLockerPass) {
+        if (studyCafePass == null) {
+            throw new AppException("StudyCafeLockerPass cannot be null");
+        }
+
+        this.studyCafePass = studyCafePass;
+        this.studyCafeLockerPass = studyCafeLockerPass;
+    }
+
+    public static StudyCafeOrder of(StudyCafePass studyCafePass, StudyCafeLockerPass studyCafeLockerPass) {
+        return new StudyCafeOrder(studyCafePass, studyCafeLockerPass);
+    }
+
+    public StudyCafePass getStudyCafePass() {
+        return studyCafePass;
+    }
+
+    public Optional<StudyCafeLockerPass> getStudyCafeLockerPass() {
+        return Optional.ofNullable(studyCafeLockerPass);
+    }
+}
